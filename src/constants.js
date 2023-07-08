fdf10be0d9837b1b93e93e91aa862f13f54e7baa
goffygoo <jrvineetoli52@gmail.com>
2023-07-08 16:47:45 +0530

    google auth

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+export const LOCALSTORAGE_KEYS = {
+    googleAuthRedirect: 'googleAuthRedirect',
+    googleAuthAction: 'googleAuthAction',
+}
+
+export const GOOGLE_AUTH_ACTION = {
+    auth: 'auth',
+    login: 'login',
+}
