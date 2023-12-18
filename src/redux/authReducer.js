43603e86fd9f41cf10e7e27a972e55e92918d90e
Vidit Pushkarna <viditpushkarna@Vidits-MacBook-Pro.local>
2023-12-18 21:32:44 +0530

    remove unnecessary logs

--- src/redux/authReducer.js
+++ src/redux/authReducer.js
@@ -1,55 +1,54 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 const getGoogleAuthLocalStorage = () => {
-  try {
-    return JSON.parse(localStorage.getItem("googleAuth"));
-  } catch (err) {
-    return null;
-  }
+	try {
+		return JSON.parse(localStorage.getItem('googleAuth'));
+	} catch (err) {
+		return null;
+	}
 };
 
 // TODO: JSON.parse global fail safe
 const initialState = {
-  accessToken: localStorage.getItem("accessToken"),
-  dataToken: localStorage.getItem("dataToken"),
-  refreshToken: localStorage.getItem("refreshToken"),
-  userId: localStorage.getItem("userId"),
-  profileId: localStorage.getItem("profileId"),
-  type: localStorage.getItem("type"),
-  email: localStorage.getItem("email"),
-  googleAuth: getGoogleAuthLocalStorage(),
+	accessToken: localStorage.getItem('accessToken'),
+	dataToken: localStorage.getItem('dataToken'),
+	refreshToken: localStorage.getItem('refreshToken'),
+	userId: localStorage.getItem('userId'),
+	profileId: localStorage.getItem('profileId'),
+	type: localStorage.getItem('type'),
+	email: localStorage.getItem('email'),
+	googleAuth: getGoogleAuthLocalStorage(),
 };
 
 export const authSlice = createSlice({
-  name: "auth",
-  initialState,
-  reducers: {
-    setAuthKey: (state, action) => {
-      const [key, value] = action.payload;
-      console.log(action.payload);
-      state[key] = value;
-      localStorage.setItem(key, value);
-    },
-    resetAuth: (state) => {
-      for (const key in state) {
-        localStorage.removeItem(key);
-        state[key] = undefined;
-      }
-    },
-    setGoogleAuth: (state, action) => {
-      const googleAuth = action.payload;
+	name: 'auth',
+	initialState,
+	reducers: {
+		setAuthKey: (state, action) => {
+			const [key, value] = action.payload;
+			state[key] = value;
+			localStorage.setItem(key, value);
+		},
+		resetAuth: (state) => {
+			for (const key in state) {
+				localStorage.removeItem(key);
+				state[key] = undefined;
+			}
+		},
+		setGoogleAuth: (state, action) => {
+			const googleAuth = action.payload;
 
-      state.googleAuth = googleAuth;
-      localStorage.setItem("googleAuth", JSON.stringify(googleAuth));
-    },
-  },
+			state.googleAuth = googleAuth;
+			localStorage.setItem('googleAuth', JSON.stringify(googleAuth));
+		},
+	},
 });
 
 export const { setAuthKey, resetAuth, setGoogleAuth } = authSlice.actions;
 
 /**
- * 
- * @deprecated 
+ *
+ * @deprecated
  * use local storage instead
  */
 export const authKeySelector = (key) => (state) => state.auth[key];
