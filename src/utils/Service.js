5adcb41fc2b223f0e8dec942fc7462395e0b27ab
Vidit Pushkarna <viditpushkarna@Vidits-MacBook-Pro.local>
2023-12-24 13:12:06 +0530

    add create community page

--- src/utils/Service.js
+++ src/utils/Service.js
@@ -1,156 +1,160 @@
-import axios from "axios";
-import config from "../config.json";
+import axios from 'axios';
+import config from '../config.json';
 
 const SERVER = config.SERVER;
 const FILE_SERVER = config.FILE_SERVER;
 
 const routeUpdateRequired = (method) => {
-  return method === "get" || method === "delete";
+	return method === 'get' || method === 'delete';
 };
 
 const getUpdatedRoute = (route, body) => {
-  let newRoute = route;
+	let newRoute = route;
 
-  const keys = Object.keys(body);
+	const keys = Object.keys(body);
 
-  if (keys.length > 0) {
-    newRoute = newRoute + "?";
+	if (keys.length > 0) {
+		newRoute = newRoute + '?';
 
-    for (let i = 0; i < keys.length; i++) {
-      newRoute =
-        newRoute +
-        encodeURIComponent(keys[i]) +
-        "=" +
-        encodeURIComponent(body[keys[i]]) +
-        "&";
-    }
+		for (let i = 0; i < keys.length; i++) {
+			newRoute =
+				newRoute +
+				encodeURIComponent(keys[i]) +
+				'=' +
+				encodeURIComponent(body[keys[i]]) +
+				'&';
+		}
 
-    newRoute = newRoute.slice(0, -1);
-  }
+		newRoute = newRoute.slice(0, -1);
+	}
 
-  return newRoute;
+	return newRoute;
 };
 const refresh_access_token = async (navigate) => {
-  const refreshToken = localStorage.getItem("refreshToken");
-  const userId = localStorage.getItem("userId");
-  try {
-    const data = await axios.post(`${SERVER}/api/auth/access/newAccessToken`, {
-      userId,
-      refreshToken,
-    });
-    localStorage.setItem("accessToken", data.data.accessToken);
-    return true;
-  } catch {
-    navigate("/login");
-  }
+	const refreshToken = localStorage.getItem('refreshToken');
+	const userId = localStorage.getItem('userId');
+	if (!userId || !refreshToken) {
+		navigate('/login');
+		return;
+	}
+	try {
+		const data = await axios.post(`${SERVER}/api/auth/access/newAccessToken`, {
+			userId,
+			refreshToken,
+		});
+		localStorage.setItem('accessToken', data.data.accessToken);
+		return true;
+	} catch {
+		navigate('/login');
+	}
 };
 export const auth_request_with_access_token = async (
-  method,
-  route,
-  body,
-  onSuccess,
-  onError
+	method,
+	route,
+	body,
+	onSuccess,
+	onError
 ) => {
-  const token = localStorage.getItem("accessToken");
+	const token = localStorage.getItem('accessToken');
 
-  const config = {
-    headers: {
-      Authorization: token,
-    },
-  };
+	const config = {
+		headers: {
+			Authorization: token,
+		},
+	};
 
-  if (routeUpdateRequired(method)) {
-    route = getUpdatedRoute(route, body);
-    body = config;
-  }
+	if (routeUpdateRequired(method)) {
+		route = getUpdatedRoute(route, body);
+		body = config;
+	}
 
-  axios[method](`${SERVER}${route}`, body, config)
-    .then((response) => {
-      onSuccess(response);
-    })
-    .catch((err) => {
-      onError(err);
-    });
+	axios[method](`${SERVER}${route}`, body, config)
+		.then((response) => {
+			onSuccess(response);
+		})
+		.catch((err) => {
+			onError(err);
+		});
 };
 
 export const auth_request = async (method, route, body, onSuccess, onError) => {
-  const config = {};
+	const config = {};
 
-  if (routeUpdateRequired(method)) {
-    route = getUpdatedRoute(route, body);
-    body = config;
-  }
+	if (routeUpdateRequired(method)) {
+		route = getUpdatedRoute(route, body);
+		body = config;
+	}
 
-  axios[method](`${SERVER}${route}`, body, config)
-    .then((response) => {
-      onSuccess(response);
-    })
-    .catch((err) => {
-      onError(err);
-    });
+	axios[method](`${SERVER}${route}`, body, config)
+		.then((response) => {
+			onSuccess(response);
+		})
+		.catch((err) => {
+			onError(err);
+		});
 };
 
 export const resource_request_with_access_token =
-  (navigate) =>
-  async (method, route, body, onSuccess, onError, level = 0) => {
-    const token = localStorage.getItem("accessToken");
-    const config = {
-      headers: {
-        Authorization: token,
-      },
-    };
-    if (routeUpdateRequired(method)) {
-      route = getUpdatedRoute(route, body);
-      body = config;
-    }
+	(navigate) =>
+	async (method, route, body, onSuccess, onError, level = 0) => {
+		const token = localStorage.getItem('accessToken');
+		const config = {
+			headers: {
+				Authorization: token,
+			},
+		};
+		if (routeUpdateRequired(method)) {
+			route = getUpdatedRoute(route, body);
+			body = config;
+		}
 
-    axios[method](`${SERVER}${route}`, body, config)
-      .then((response) => {
-        onSuccess(response);
-      })
-      .catch(async (err) => {
-        if (err?.response?.data?.invalid) {
-          await refresh_access_token(navigate);
-          if (level >= 5) onError(err);
-          else
-            resource_request_with_access_token(navigate)(
-              method,
-              route,
-              body,
-              onSuccess,
-              onError,
-              level + 1
-            );
-        } else onError(err);
-      });
-  };
+		axios[method](`${SERVER}${route}`, body, config)
+			.then((response) => {
+				onSuccess(response);
+			})
+			.catch(async (err) => {
+				if (err?.response?.data?.invalid) {
+					await refresh_access_token(navigate);
+					if (level >= 5) onError(err);
+					else
+						resource_request_with_access_token(navigate)(
+							method,
+							route,
+							body,
+							onSuccess,
+							onError,
+							level + 1
+						);
+				} else onError(err);
+			});
+	};
 
 export const file_server_request = async (
-  method,
-  route,
-  body,
-  onSuccess,
-  onError
+	method,
+	route,
+	body,
+	onSuccess,
+	onError
 ) => {
-  const token = localStorage.getItem("accessToken");
+	const token = localStorage.getItem('accessToken');
 
-  const config = {
-    headers: {
-      Authorization: token,
-      "Content-Type": "multipart/form-data",
-    },
-  };
+	const config = {
+		headers: {
+			Authorization: token,
+			'Content-Type': 'multipart/form-data',
+		},
+	};
 
-  if (routeUpdateRequired(method)) {
-    route = getUpdatedRoute(route, body);
-    body = config;
-  }
+	if (routeUpdateRequired(method)) {
+		route = getUpdatedRoute(route, body);
+		body = config;
+	}
 
-  axios[method](`${FILE_SERVER}${route}`, body, config)
-    .then((response) => {
-      onSuccess(response);
-    })
-    .catch((err) => {
-      onError(err);
-    });
+	axios[method](`${FILE_SERVER}${route}`, body, config)
+		.then((response) => {
+			onSuccess(response);
+		})
+		.catch((err) => {
+			onError(err);
+		});
 };
